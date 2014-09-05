9188b942fe62e2a02f5c2bbf1f83d07325cdb5f9
mmilligan <mmilligan@ubabenefits.com>
2014-09-05 15:47:22 -0400

    onclick

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -128,7 +128,7 @@ angular.module('starter.controllers', ['ionic'])
         };
         
         $scope.openUrl = function() {
-    		var ref = $window.open('http://mobile.ubabenefits.com/UBAConIntro/UBAConPresentations/tabid/137/Default.aspx', '_blank', 'location=no', 'hidden=no', 'closebuttoncaption=Done', 'toolbar=yes', 'toolbarposition=top');
+    		var ref = $window.open('http://mobile.ubabenefits.com/UBAConIntro/UBAConPresentations/tabid/137/Default.aspx', '_blank', 'location=no');
     		ref.addEventListener('loadstart', function(event) { alert('start: ' + event.url); });
          	ref.addEventListener('loadstop', function(event) { alert('stop: ' + event.url); });
          	ref.addEventListener('loaderror', function(event) { alert('error: ' + event.message); });
